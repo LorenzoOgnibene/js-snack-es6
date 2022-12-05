339665f4ffbfbe4db81cf9735c92b59a65a63d63
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2022-12-05 12:30:09 +0100

    mi piace la zucchina ah ah ah

---
+++ snack_3/js/javaScript.js
@@ -0,0 +1,72 @@
+/**
+ *
+ * Crea 10 oggetti che rappresentano una zucchina, 
+ * indicandone per ognuno varietà, peso e lunghezza.
+ *
+ * Calcola quanto pesano tutte le zucchine.
+ *
+ */
+
+
+
+const zucchine = [
+    {
+        variety : 'cinese',
+        weight : 120,
+        lenght : 12
+    },
+    {
+        variety : 'cinese',
+        weight : 12,
+        lenght : 12
+    },
+    {
+        variety : 'cinese',
+        weight : 30,
+        lenght : 12
+    },
+    {
+        variety : 'cinese',
+        weight : 450,
+        lenght : 12
+    },
+    {
+        variety : 'cinese',
+        weight : 333,
+        lenght : 12
+    },
+    {
+        variety : 'cinese',
+        weight : 232,
+        lenght : 12
+    },
+    {
+        variety : 'cinese',
+        weight : 121,
+        lenght : 12
+    },
+    {
+        variety : 'cinese',
+        weight : 88,
+        lenght : 12
+    },
+    {
+        variety : 'cinese',
+        weight : 69,
+        lenght : 12
+    },
+    {
+        variety : 'cinese',
+        weight : 64,
+        lenght : 12
+    },
+]
+
+sum = 0;
+
+for(let i = 0; i < zucchine.length; i++){
+    console.log(zucchine[i].weight)
+    sum += zucchine[i].weight;
+}
+
+console.log(sum)
